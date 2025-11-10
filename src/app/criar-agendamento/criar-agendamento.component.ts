f95622793231937de43268847f5f32bebb40f809
Guilherme Vilã <guilhermesnts.profissional@gmail.com>
2025-11-10 06:34:44 -0300

    html concluido

---
+++ src/app/criar-agendamento/criar-agendamento.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-agendamento',
+  imports: [],
+  templateUrl: './criar-agendamento.component.html',
+  styleUrl: './criar-agendamento.component.css'
+})
+export class CriarAgendamentoComponent {
+
+}
